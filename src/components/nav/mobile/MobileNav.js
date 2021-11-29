e4f3840a1aa9d59e2e57602ef6ddc9456b94ea22
junescarlet <95093740+junescarlet@users.noreply.github.com>
2021-11-29 14:20:00 -0600

    Added Sidebar

--- src/components/nav/mobile/MobileNav.js
+++ src/components/nav/mobile/MobileNav.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 import classes from './MobileNav.module.scss'
+import Sidebar from '../sidebar/Sidebar';
 
 
 const MobileNav = props => {
-    
+    const [displaySidebar, setDisplaySidebar] = useState(false);
 
+    const displaySidebarHandler = () => {
+        setDisplaySidebar(!displaySidebar);
+    };
 
     return (
         <>
             <div className={classes.mobileNav}>
+            {displaySidebar &&  <><div className={classes.backdrop} onClick={displaySidebarHandler} /> <Sidebar /> </>}
                 <ul>
-                    <li><s>Menu</s></li>
+                    <li>
+                        <Button variant="link" onClick={displaySidebarHandler} >
+                            <br />Menu
+                        </Button>
+                    </li>
                     <li><s>Prev</s></li>
                     <li><s>Back</s></li>
                     <li><s>Next</s></li>
---
+++ src/components/nav/sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import classes from './Sidebar.module.scss'
+
+const Sidebar = (props) => {
+    
+
+    return (
+        <nav className={classes.sidebar}>
+            <ul>
+               
+            </ul>
+            <br /><br /><br /><br />
+        </nav>
+    );
+};
+
+export default Sidebar;
