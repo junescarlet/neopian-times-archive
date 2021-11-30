28b8337859c42d721d5c86c0dc46e9ddc0557325
junescarlet <95093740+junescarlet@users.noreply.github.com>
2021-11-30 14:29:35 -0600

    Working on styling Nav with Bootstrap

--- src/components/nav/mobile/MobileNav.js
+++ src/components/nav/mobile/MobileNav.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 import classes from './MobileNav.module.scss'
 import Sidebar from '../sidebar/Sidebar';
@@ -14,7 +16,41 @@ const MobileNav = props => {
 
     return (
         <>
-            <div className={classes.mobileNav}>
+            {displaySidebar &&  <><div className={classes.backdrop} onClick={displaySidebarHandler} /> <Sidebar /> </>}
+            <Navbar fixed="bottom">
+            <Nav>
+                <Nav.Item>
+                    <Button variant="link" onClick={displaySidebarHandler} >
+                        <br />Menu
+                    </Button>
+                </Nav.Item>
+                <Nav.Item>
+                    <s>Prev</s>
+                </Nav.Item>
+                <Nav.Item>
+                    <s>Back</s>
+                </Nav.Item>
+                <Nav.Item>
+                    <s>Next</s>
+                </Nav.Item>
+                <Nav.Item>
+                    <s>Search</s>
+                </Nav.Item>
+            </Nav>
+            
+            {/* <ul>
+                    <li>
+                        <Button variant="link" onClick={displaySidebarHandler} >
+                            <br />Menu
+                        </Button>
+                    </li>
+                    <li><s>Prev</s></li>
+                    <li><s>Back</s></li>
+                    <li><s>Next</s></li>
+                    <li><s>Search</s></li>
+                </ul> */}
+            </Navbar>
+            {/* <div className={classes.mobileNav}>
             {displaySidebar &&  <><div className={classes.backdrop} onClick={displaySidebarHandler} /> <Sidebar /> </>}
                 <ul>
                     <li>
@@ -27,7 +63,7 @@ const MobileNav = props => {
                     <li><s>Next</s></li>
                     <li><s>Search</s></li>
                 </ul>
-            </div>
+            </div> */}
         </>
     );
 };
--- src/components/nav/sidebar/Sidebar.js
+++ src/components/nav/sidebar/Sidebar.js
@@ -9,7 +9,17 @@ const Sidebar = (props) => {
     return (
         <nav className={classes.sidebar}>
             <ul>
-               
+               <li><s>Home</s></li>
+               <li><s>Archives</s></li>
+               <li><s>Editorial</s></li>
+               <li><s>Articles</s></li>
+               <li><s>Comics</s></li>
+               <li><s>Short Stories</s></li>
+               <li><s>New Series</s></li>
+               <li><s>Continued Series</s></li>
+               <li><s>Classic Mode</s></li>
+               <li><s>About</s></li>
+               <li><s>Contact</s></li>
             </ul>
             <br /><br /><br /><br />
         </nav>
