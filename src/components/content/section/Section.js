b5bc0e13b1676593ea1cb055a343a9298ad9050a
junescarlet <95093740+junescarlet@users.noreply.github.com>
2021-12-19 15:45:51 -0600

    currently trying to figure out file extensions

--- src/components/content/section/Section.js
+++ src/components/content/section/Section.js
@@ -1,4 +1,4 @@
-import { React, useEffect }from 'react';
+import { React }from 'react';
 import { useParams } from "react-router-dom";
 
 import './Section.scss';
@@ -10,41 +10,17 @@ import Thumbnails from '../thumbnails/Thumbnails';
 const Section = props => {
     let {timesData} = props;
     let params = useParams();
-    // let sectionsArray = Object.keys(timesData).map((keyName, i) => (
-    //     { keyName }
-    // ));
-
-    // useEffect(() => {
-    //     fetch("https://api.jsonbin.io/b/617849249548541c29c8ed3e")
-    //       .then(response => response.json())
-    //       .then(data => { 
-    //         setStaticComicData(data);
-    //       })
-    //       .catch(err =>{ console.error(err => console.error(err))}); 
-    //     }, []);
     
     
     let getSection = (id) => {
-        //returns four times? twice undefined, and twice with the params
-        //return Object.keys(timesData).map((keyName, i) => ()) === params;
-        //return only the object where the keyName matches the params
-        //return Object.keys(timesData);
-        // timesData.filter()
         for (let item in timesData) {
             if (item === id) {
                 return timesData[item];
             }
         }
     }
-    // console.log(sectionsArray);
 
-    // let getSection = (id) => {
-    //     return sectionsArray.find(
-    //         section => timesData.id === id
-    //     );
-    // }
     let section = getSection(params.sectionId);
-    //console.log(section)
     return (
         <div key={section} className="section">
             <h1>{params.sectionId}</h1>
@@ -53,28 +29,6 @@ const Section = props => {
                     <Thumbnails key={piece.Filename} sectionData={piece} />
                 </div>
             ): <p>Loading</p>}
-            {/* {Object.keys(timesData).map((keyName, i) => (
-                <div key={keyName}>
-                    <h1>{keyName}</h1>
-                    {timesData[keyName].map((piece) => 
-                    <>
-                    <Thumbnails key={piece.Filename} sectionData={piece} />
-               </>
-                    )}
-                                   
-                    {timesData[keyName].map((piece) => 
-                    <div key={piece.Filename}>
-                        <h2>{piece.Name}</h2>
-                        <p>{piece.Desc}</p>
-                        <hr />
-                    </div>
-                    )} 
-                </div>
-            ))} */}
-            
-            <p>
-            </p>
-            
         </div>
     );
 };
--- src/components/content/section/comics/comic/Comic.js
+++ src/components/content/section/comics/comic/Comic.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+import { existsSync } from 'fs';
 
 import './Comic.scss';
 
 
 const Comic = props => {
-    
     let { timesData } = props;
     let params = useParams();
+    const fs = require("fs");
 
     
     let getPiece = (id, array) => {
@@ -17,9 +18,20 @@ const Comic = props => {
             );
         }
     }
-   
+    
+    function getFileExtension(filename) {
+        //return filename.split('.').pop()
+        if (fs.existsSync(`{filename}.jpg`)) {
+            return "jpg";
+        } else {
+            return "gif";
+        }
+    }
+
+    
     
     let piece = getPiece(params.pieceId, timesData);
+    timesData && console.log(getFileExtension(`archive/NT_archive_946/946/comics/${piece.Filename}`));
     return (
         <div className="comic">
     
@@ -28,6 +40,7 @@ const Comic = props => {
                     <h1> My Comic: {piece.Name}</h1>
                     <p>by {piece.UN[0]} {piece.UN[1]} {piece.UN[2]}</p>
                     <p>{params.pieceId}</p>
+                    <img src={`archive/NT_archive_946/946/comics/${piece.Filename}.jpg`} />
                 </>
                 :
                 <p>Loading</p>
