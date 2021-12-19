7ee08c4da3139d00ff1cff992f4bff49e81818b5
junescarlet <95093740+junescarlet@users.noreply.github.com>
2021-12-19 14:36:58 -0600

    Can refresh the page now!

--- src/components/content/section/comics/comic/Comic.js
+++ src/components/content/section/comics/comic/Comic.js
@@ -1,59 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from "react-router-dom";
 
 import './Comic.scss';
 
 
 const Comic = props => {
-    //const [piece, setPiece] = useState("");
+    
     let { timesData } = props;
     let params = useParams();
 
-    //console.log(timesData);
-    //console.log(params);
-
-    // export function getInvoice(number) {
-    //     return invoices.find(
-    //       invoice => invoice.number === number
-    //     );
-    //   }
-    // useEffect(() => {
-    //     document.title = `You clicked ${count} times`;
-    //   }, [count]);
-      
-   // useEffect(() => {
-        // let getPiece = (id) => {
-        //     return timesData.find(
-        //         comic => comic.Filename === id
-        //     );
-        // }
-        // let getPiece = (id) => {
-        //     setPiece(timesData.find(
-        //         comic => comic.Filename === id
-        //     ));
-        // }
-        // setPiece(timesData.find(
-        //     comic => comic.Filename === id
-        // ));
-    //}, []);
-
+    
     let getPiece = (id, array) => {
-        return array.find(
-            piece => piece.Filename === id
-        );
+        if (array) {
+            return array.find(
+                piece => piece.Filename === id
+            );
+        }
     }
    
     
     let piece = getPiece(params.pieceId, timesData);
-    //setPiece(getPiece(params.pieceId));
-    //getPiece(params.pieceId);
-    //console.log(piece);
     return (
         <div className="comic">
     
             {timesData ?  
                 <>
                     <h1> My Comic: {piece.Name}</h1>
+                    <p>by {piece.UN[0]} {piece.UN[1]} {piece.UN[2]}</p>
                     <p>{params.pieceId}</p>
                 </>
                 :
