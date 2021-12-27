d7b9f8a8da74b94ddec3c30c9e0419f0d4892cbb
junescarlet <95093740+junescarlet@users.noreply.github.com>
2021-12-26 23:09:34 -0600

    Added html-loader

--- src/components/content/section/Text/Text.js
+++ src/components/content/section/Text/Text.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+//import Page from './test.html'
+
+//import html from "./file.html";
+
+//var __html = require(`archive/NT_archive_946/946/articles/585474.html`);
+
 
 //import './Text.scss';
 
@@ -7,6 +13,7 @@ import { useParams } from "react-router-dom";
 const Comic = props => {
     let { timesData } = props;
     let params = useParams();
+    
     
     let getPiece = (id, array) => {
         if (array) {
@@ -17,6 +24,16 @@ const Comic = props => {
     }
     
     let piece = getPiece(params.pieceId, timesData);
+    
+    function createMarkup(Filename) {
+        if(timesData) {
+            return {__html: `archive/NT_archive_946/946/articles/${Filename}.html`};
+        }
+        
+      }
+
+      
+
     return (
         <div className="comic">
     
@@ -27,7 +44,8 @@ const Comic = props => {
                     <p>{params.pieceId}</p>
                     <img alt={piece.Name} src={piece.Image} />
                     {/* <img alt="{piece.Name}" src={`archive/NT_archive_946/946/comics/${piece.Filename}.jpg`} /> */}
-                    {`archive/NT_archive_946/946/articles/${piece.Filename}.html`}
+                    {/* {`archive/NT_archive_946/946/articles/${piece.Filename}.html`} */}
+                    <div dangerouslySetInnerHTML={createMarkup(piece.Filename)} />
                 </>
                 :
                 <p>Loading</p>
